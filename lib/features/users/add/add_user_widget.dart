8c9ab50696d9273ea30e1d9a46a962b6374143ee
Saran <saran.mampatta90@gmail.com>
2025-10-21 16:13:11 +0530

    fix: keyboard overlap issue in add/update user

--- lib/features/users/add/add_user_widget.dart
+++ lib/features/users/add/add_user_widget.dart
@@ -16,65 +16,68 @@ class AddUserWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
-        gradient: LinearGradient(
-          colors: [appColorGradient1, appColorGradient2],
-          begin: Alignment.topLeft,
-          stops: [0.5, 1],
-          end: Alignment.bottomRight,
+    return Padding(
+      padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+      child: Container(
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+          gradient: LinearGradient(
+            colors: [appColorGradient1, appColorGradient2],
+            begin: Alignment.topLeft,
+            stops: [0.5, 1],
+            end: Alignment.bottomRight,
+          ),
         ),
-      ),
-      padding: const EdgeInsets.all(24),
-      height: 400,
-      child: Stack(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Align(
-                alignment: Alignment.center,
-                child: Text(
-                  user == null ? 'Add User' : 'Update User',
-                  style: const TextStyle(
-                    fontSize: 21,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.white,
+        padding: const EdgeInsets.all(24),
+        height: 400,
+        child: Stack(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    user == null ? 'Add User' : 'Update User',
+                    style: const TextStyle(
+                      fontSize: 21,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 30),
-              IconTextField(
-                controller: controller.userNameController,
-                hint: 'Enter username',
-                textInputType: TextInputType.text,
-                whiteBackground: false,
-                label: 'Username',
-              ),
-              const SizedBox(height: 10),
-              IconTextField(
-                controller: controller.passwordController,
-                hint: 'Enter password',
-                textInputType: TextInputType.text,
-                whiteBackground: false,
-                label: 'Password',
-              ),
-            ],
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Padding(
-              padding: const EdgeInsets.only(bottom: 30),
-              child: AppButton(
-                label: 'Save',
-                onSubmit: controller.onSaveClicked,
-                startColor: appColorGradient1,
-                endColor: appColorGradient2,
-              ),
+                const SizedBox(height: 30),
+                IconTextField(
+                  controller: controller.userNameController,
+                  hint: 'Enter username',
+                  textInputType: TextInputType.text,
+                  whiteBackground: false,
+                  label: 'Username',
+                ),
+                const SizedBox(height: 10),
+                IconTextField(
+                  controller: controller.passwordController,
+                  hint: 'Enter password',
+                  textInputType: TextInputType.text,
+                  whiteBackground: false,
+                  label: 'Password',
+                ),
+              ],
             ),
-          )
-        ],
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 30),
+                child: AppButton(
+                  label: 'Save',
+                  onSubmit: controller.onSaveClicked,
+                  startColor: appColorGradient1,
+                  endColor: appColorGradient2,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }

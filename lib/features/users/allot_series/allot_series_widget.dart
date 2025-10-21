9f141b32f4fc87cc8904df82bef7dc1807b5c26d
Saran <saran.mampatta90@gmail.com>
2025-10-21 16:11:49 +0530

    fix: keyboard overlap issue in allot series

--- lib/features/users/allot_series/allot_series_widget.dart
+++ lib/features/users/allot_series/allot_series_widget.dart
@@ -16,79 +16,82 @@ class AllotSeriesWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
-        gradient: LinearGradient(
-          colors: [appColorGradient1, appColorGradient2],
-          begin: Alignment.topLeft,
-          stops: [0.5, 1],
-          end: Alignment.bottomRight,
+    return Padding(
+      padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+      child: Container(
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+          gradient: LinearGradient(
+            colors: [appColorGradient1, appColorGradient2],
+            begin: Alignment.topLeft,
+            stops: [0.5, 1],
+            end: Alignment.bottomRight,
+          ),
         ),
-      ),
-      padding: const EdgeInsets.all(24),
-      height: 500,
-      child: Stack(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Align(
-                alignment: Alignment.center,
-                child: Text(
-                  user == null ? 'Add User' : 'Update User',
-                  style: const TextStyle(
-                    fontSize: 21,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.white,
+        padding: const EdgeInsets.all(24),
+        height: 500,
+        child: Stack(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    user == null ? 'Add User' : 'Update User',
+                    style: const TextStyle(
+                      fontSize: 21,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 30),
-              IconTextField(
-                controller: controller.nameController,
-                hint: 'Enter name',
-                textInputType: TextInputType.text,
-                whiteBackground: false,
-                label: 'Name',
-                isEnabled: false,
-              ),
-              const SizedBox(height: 10),
-              InkWell(
-                onTap: () => controller.onBillDateClicked(context),
-                child: IconTextField(
-                  controller: controller.billDateController,
-                  hint: 'Enter bill date',
+                const SizedBox(height: 30),
+                IconTextField(
+                  controller: controller.nameController,
+                  hint: 'Enter name',
                   textInputType: TextInputType.text,
                   whiteBackground: false,
-                  label: 'Bill Date',
+                  label: 'Name',
                   isEnabled: false,
-                  suffixIcon: 'assets/icons/ic_calendar.png',
+                ),
+                const SizedBox(height: 10),
+                InkWell(
+                  onTap: () => controller.onBillDateClicked(context),
+                  child: IconTextField(
+                    controller: controller.billDateController,
+                    hint: 'Enter bill date',
+                    textInputType: TextInputType.text,
+                    whiteBackground: false,
+                    label: 'Bill Date',
+                    isEnabled: false,
+                    suffixIcon: 'assets/icons/ic_calendar.png',
+                  ),
+                ),
+                const SizedBox(height: 10),
+                IconTextField(
+                  controller: controller.seriesController,
+                  hint: 'ABC, PQR,..',
+                  textInputType: TextInputType.text,
+                  whiteBackground: false,
+                  label: 'Series',
+                ),
+              ],
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 30),
+                child: AppButton(
+                  label: 'Save',
+                  onSubmit: controller.onSaveClicked,
+                  startColor: appColorGradient1,
+                  endColor: appColorGradient2,
                 ),
               ),
-              const SizedBox(height: 10),
-              IconTextField(
-                controller: controller.seriesController,
-                hint: 'ABC, PQR,..',
-                textInputType: TextInputType.text,
-                whiteBackground: false,
-                label: 'Series',
-              ),
-            ],
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Padding(
-              padding: const EdgeInsets.only(bottom: 30),
-              child: AppButton(
-                label: 'Save',
-                onSubmit: controller.onSaveClicked,
-                startColor: appColorGradient1,
-                endColor: appColorGradient2,
-              ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
